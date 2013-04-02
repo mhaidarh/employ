607347bd349dbb253ea0bb69b8bba3b6d2fecc0a
M Haidar Hanif <mhaidarhanif@gmail.com>
2013-04-02 12:32:44 +0700

    Add label and text field with application code

--- src/DashboardFrame.java
+++ src/DashboardFrame.java
@@ -8,18 +8,40 @@ import javax.swing.*;
 
 public class DashboardFrame extends CommonFrame implements ActionListener {
 
+  JButton buttonAddData = new JButton("Add");
+  JButton buttonDeleteData = new JButton("Delete");
+  JTextField textNumber = new JTextField(10);
+  JTextField textOutput = new JTextField(10);
+
   // define dashboard for special frame
   public DashboardFrame() {
     super("Dashboard");
 
-    JButton buttonAddData = new JButton("Add");
+    buttonAddData.setActionCommand("addData");
+    buttonDeleteData.setActionCommand("deleteData");
+
     buttonAddData.addActionListener(this);
+    buttonDeleteData.addActionListener(this);
+    textNumber.addActionListener(this);
+
     add(buttonAddData);
+    add(buttonDeleteData);
+    add(textNumber);
+    add(textOutput);
   }
 
   // define listener action when button "Add" is clicked
   public void actionPerformed(ActionEvent event) {
-    getContentPane().setBackground(Color.white);
+    String inputNumber = textNumber.getText();
+    textOutput.setText(inputNumber);
+
+    if (event.getActionCommand().equals("addData"))
+      getContentPane().setBackground(Color.white);
+    else if (event.getActionCommand().equals("deleteData"))
+      getContentPane().setBackground(Color.darkGray);
+    else
+      getContentPane().setBackground(Color.black);
+
     repaint();
   }
 
