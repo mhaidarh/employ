24be61fa9621d567fb9ede3a063249fa8ed46eed
M Haidar Hanif <mhaidarhanif@gmail.com>
2013-04-03 12:33:03 +0700

    Optimize application logic
    Define all earnings logic in README.
    Implement radio button to get selected value.

--- src/CommonFrame.java
+++ src/CommonFrame.java
@@ -2,6 +2,9 @@
  * Part of Employ
  */
 
+import java.awt.*;
+import java.util.Enumeration;
+
 import javax.swing.*;
 
 public abstract class CommonFrame extends JFrame {
@@ -12,16 +15,27 @@ public abstract class CommonFrame extends JFrame {
   public CommonFrame(String heading) {
     super("Employ - " + heading);
 
-    setSize(400, 200);
+    setSize(300, 400);
     setResizable(false);
     setLocationRelativeTo(null);
-    setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+    setLayout(new FlowLayout());
 
     JLabel labelHeading = new JLabel(heading);
     panelHeading.add(labelHeading);
     add(panelHeading);
 
     setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
+
+  // custom method to get text of the selected button
+  public String getSelectedButtonText(ButtonGroup buttonGroup) {
+    Enumeration<AbstractButton> buttons;
+    for (buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
+      AbstractButton button = buttons.nextElement();
+      if (button.isSelected())
+        return button.getText();
+    }
+    return null;
   }
 
 }
--- src/DashboardFrame.java
+++ src/DashboardFrame.java
@@ -9,21 +9,31 @@ import javax.swing.*;
 
 public class DashboardFrame extends CommonFrame implements ActionListener {
 
-  JPanel panelFieldInput = new JPanel();
-  JPanel panelFieldOutput = new JPanel();
+  JPanel panelInput = new JPanel();
+  JPanel panelType = new JPanel();
   JPanel panelButton = new JPanel();
+  JPanel panelOutput = new JPanel();
 
   JLabel labelNumberInput = new JLabel("Number Input: ");
-  JTextField textNumberInput = new JTextField(20);
+  JTextField textNumberInput = new JTextField(10);
+
+  ButtonGroup employeeType = new ButtonGroup();
+  JRadioButton eSalaried = new JRadioButton("Salaried", true);
+  JRadioButton eHourly = new JRadioButton("Hourly", false);
+  JRadioButton eCommission = new JRadioButton("Commission", false);
+  JRadioButton eCommissionPlus = new JRadioButton("Commission Plus", false);
+
   JLabel labelNumberOutput = new JLabel("Number Output: ");
-  JTextField textNumberOutput = new JTextField(20);
+  JTextField textNumberOutput = new JTextField(10);
+  JLabel labelTypeOutput = new JLabel("Employee Type: ");
+  JTextField textTypeOutput = new JTextField(15);
 
   JButton buttonAddData = new JButton("Add");
   JButton buttonDeleteData = new JButton("Delete");
   JButton buttonClearData = new JButton("Clear");
 
   double numberInput = 0;
-  String warnInputNumber = "Type number (dot-decimal)";
+  String warnInput = "Type number (dot-decimal)";
 
   // define dashboard for special frame
   public DashboardFrame() {
@@ -39,61 +49,83 @@ public class DashboardFrame extends CommonFrame implements ActionListener {
     buttonClearData.addActionListener(this);
     textNumberOutput.setEditable(false);
 
-    // put component on panel on frame
-    panelFieldInput.add(labelNumberInput);
-    panelFieldInput.add(textNumberInput);
-    panelFieldOutput.add(labelNumberOutput);
-    panelFieldOutput.add(textNumberOutput);
+    eSalaried.setActionCommand("typeSalaried");
+    eHourly.setActionCommand("typeHourly");
+    eCommission.setActionCommand("typeCommission");
+    eCommissionPlus.setActionCommand("typeCommissionPlus");
+    employeeType.add(eSalaried);
+    employeeType.add(eHourly);
+    employeeType.add(eCommission);
+    employeeType.add(eCommissionPlus);
+    panelType.setLayout(new BoxLayout(panelType, BoxLayout.Y_AXIS));
+    panelType.add(eSalaried);
+    panelType.add(eHourly);
+    panelType.add(eCommission);
+    panelType.add(eCommissionPlus);
 
+    panelInput.add(labelNumberInput);
+    panelInput.add(textNumberInput);
+
+    panelButton.setLayout(new BoxLayout(panelButton, BoxLayout.X_AXIS));
     panelButton.add(buttonAddData);
     panelButton.add(buttonDeleteData);
     panelButton.add(buttonClearData);
 
-    add(panelFieldInput);
-    add(panelFieldOutput);
+    panelOutput.setLayout(new BoxLayout(panelOutput, BoxLayout.Y_AXIS));
+    panelOutput.add(labelNumberOutput);
+    panelOutput.add(textNumberOutput);
+    panelOutput.add(labelTypeOutput);
+    panelOutput.add(textTypeOutput);
+
+    add(panelInput);
+    add(panelType);
     add(panelButton);
+    add(panelOutput);
   }
 
   // get number input in text input
   // also define exception if input is not number
-  void getNumberInput() {
+  void getInputAll() {
     String numberInputString = textNumberInput.getText();
     try {
       numberInput = Double.parseDouble(numberInputString);
       textNumberOutput.setText(numberInput + "");
+      textTypeOutput.setText(getSelectedButtonText(employeeType));
     } catch (Exception ex) {
-      textNumberOutput.setText(warnInputNumber);
+      textNumberOutput.setText(warnInput);
     }
   }
 
   // clear number output in text output
-  void clearNumberOutput() {
+  void clearOutputAll() {
     textNumberOutput.setText("");
+    textTypeOutput.setText("");
   }
 
   // clear number input & output in text input & output
-  void clearNumberInputOutput() {
+  void clearInputOutputAll() {
     textNumberInput.setText("");
     textNumberOutput.setText("");
+    textTypeOutput.setText("");
   }
 
   // give warning message in text output if number input is empty
-  void warnInputNumber() {
-    textNumberOutput.setText(warnInputNumber);
+  void warnInput() {
+    textNumberOutput.setText(warnInput);
   }
 
   // define listener action when button is clicked
   public void actionPerformed(ActionEvent event) {
     if (event.getActionCommand().equals("addData")) {
       if (textNumberInput.getText().equals("")) {
-        warnInputNumber();
+        warnInput();
       } else {
-        getNumberInput();
+        getInputAll();
       }
     } else if (event.getActionCommand().equals("deleteData")) {
-      clearNumberOutput();
+      clearOutputAll();
     } else if (event.getActionCommand().equals("clearData")) {
-      clearNumberInputOutput();
+      clearInputOutputAll();
     } else {
       textNumberOutput.setText("Use the button!");
     }
